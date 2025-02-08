3b2bd90c6f834c009a303312a7b1637648da02a4
BhumikaKC <bhumikakc09@gmail.com>
2025-02-08 21:42:02 +0545

    Updated the API endpoints

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,6 +1,6 @@
 class ApiEndPoints{
-  static const baseUrl = "http://10.0.2.2:3001";
-  // static const baseUrl = "http://172.25.5.165:3001";
+  // static const baseUrl = "http://10.0.2.2:3001";
+  static const baseUrl = "http://172.25.5.165:3001";
 
   //authentication
   static const login = "${baseUrl}/api/auth/login";
