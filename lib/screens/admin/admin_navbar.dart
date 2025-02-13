5cbf49b5e4b3a77ccc629a6bea65e8bd869daf8f
BhumikaKC <bhumikakc09@gmail.com>
2025-02-13 23:42:55 +0545

    added comments

--- lib/screens/admin/admin_navbar.dart
+++ lib/screens/admin/admin_navbar.dart
@@ -25,7 +25,7 @@ class AdminNavBar extends StatelessWidget {
           _buildNavItem(Icons.event, "Manage Events", 1),
           _buildNavItem(Icons.feedback, "User Feedback", 2),
           _buildNavItem(Icons.settings, "Settings", 3),
-          _buildNavItem(Icons.settings, "Settings", 4),
+          _buildNavItem(Icons.settings, "Settings", 4), //comment
         ],
       ),
     );
